288a1b0faadc6028dbb6e05d3a884307db0aa3d2
TonnyMusembi <musembutonnyj@gmail.com>
2023-04-21 17:56:50 +0300

    405

---
+++ src/views/NotFound.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { useRouter } from "vue-router";
+const router = useRouter();
+
+const handleError = () => {
+  console.log("go home");
+  router.push("/");
+};
+</script>
+<template>
+  <div class="container max-w-sm mx-auto text-center ">
+    <p class="mt-7 text-7xl font-bold">404</p>
+    <p class="mt-7 text-6xl">Ooops.</p>
+    <p class="mt-7">Not Found</p>
+    <button class="bg-teal-600 rounded-md" @click="handleError">
+      Go Home...
+    </button>
+  </div>
+</template>
